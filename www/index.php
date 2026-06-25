3cdc7c4e4f0989054fa31eafcaf8cb2577e53cb6
jgmoren <jgpmoren@gmail.com>
2026-06-25 07:42:36 -0300

    coneccao com o banco de dados

--- www/index.php
+++ www/index.php
@@ -1,13 +1,13 @@
 
 <?php
-$host = 'db'; // EM VEZ DE 'localhost', use o nome do serviço do docker-compose
+$host = 'db';
 $banco = 'copa_db';
 $usuario = 'root';
 $senha = 'root';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conectado ao MySQL com sucesso via Docker!";
 } catch (PDOException $e) {
     echo "Erro na conexão: " . $e->getMessage();
